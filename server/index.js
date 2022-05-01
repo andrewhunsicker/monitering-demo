b9e7fa50b26cf5e4fa79ad795249979815b8ff68
andrew hunsicker <ahunsicker888@gmail.com>
2022-05-01 15:32:57 -0400

    testing css

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+app.get('style', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/styles.css'))
+})
+
 app.use(express.static(path.join(__dirname, '../public')))
 
 let students = []
