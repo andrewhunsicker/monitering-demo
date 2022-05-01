d68d620c9562830208c53385179b2a99cb66dd5c
andrew hunsicker <ahunsicker888@gmail.com>
2022-05-01 14:10:18 -0400

    added rollbar and updated files created public folder

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const path = require('path')
 const app = express()
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
   accessToken: 'd214e1ef982b4cb4a0176495826c4a43',
   captureUncaught: true,
   captureUnhandledRejections: true,
@@ -14,31 +14,38 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
-    rollbar.info('file served')
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-const port = process.env.PORT || 4545
-
-// app.listen(port, () => console.log(`take us to warp ${port}`))
+app.use(express.static(path.join(__dirname, '../public')))
 
 let students = []
 
-app.post('/api/student', (req, res) => {
+app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log(`student added successfully`, {author: `Andrew`, type: `manual entry` })
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.critical('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
 
 app.use(rollbar.errorHandler())
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
