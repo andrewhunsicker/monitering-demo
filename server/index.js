6aa443cae3e3e423ecf1835ba7ef2951636d837e
andrew hunsicker <ahunsicker888@gmail.com>
2022-05-01 13:41:07 -0400

    added rollbar

--- server/index.js
+++ server/index.js
@@ -17,8 +17,28 @@ rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('file served')
 })
 
 const port = process.env.PORT || 4545
 
-app.listen(port, () => console.log(`take us to warp ${port}`))
+// app.listen(port, () => console.log(`take us to warp ${port}`))
+
+let students = []
+
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log(`student added successfully`, {author: `Andrew`, type: `manual entry` })
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
