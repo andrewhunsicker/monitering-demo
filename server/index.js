f033fe9eea2060eda610f96a0bf4f62b99ac98a9
andrew hunsicker <ahunsicker888@gmail.com>
2022-05-01 15:46:28 -0400

    trying

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-app.get('style', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/styles.css'))
-})
+// app.get('style', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/styles.css'))
+// })
 
 app.use(express.static(path.join(__dirname, '../public')))
 
